c4b55c7c8fbdb013ad374c79d4f76d3caaff3aed
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-10-19 11:15:05 +0300

    refact app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const requestLimit = require('express-rate-limit');
 const usersRouters = require('./routes/users.js');
 const cardsRouters = require('./routes/cards.js');
+const error = require('./routes/error.js');
+const { requestLogger, errorLogger } = require('./middlewares/logger.js');
+const { login, createUser } = require('./controllers/users.js');
+const auth = require('./middlewares/auth.js');
 
 const { PORT = 3000 } = process.env;
 
@@ -11,14 +18,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  req.user = {
-    id: '5f68979b35f6152688a52010',
-  };
-
-  next();
-});
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -26,11 +26,51 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+const limit = requestLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limit);
+app.use(requestLogger);
+app.use(errorLogger);
+app.use(auth);
 app.use('/', usersRouters);
 app.use('/', cardsRouters);
+app.use('*', error);
+app.use(errors());
 
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(10),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(10),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().uri(),
+  }).unknown(true),
+}), createUser);
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send(err.message);
+    return;
+  }
+  console.log(err.name);
+  res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
+  next();
 });
 
 app.listen(PORT, () => {
