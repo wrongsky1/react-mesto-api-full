cc1dca084f2fee6d00dc12e21a966366533cfe08
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-10-25 00:15:28 +0300

    fix error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-const { PORT = 6000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
